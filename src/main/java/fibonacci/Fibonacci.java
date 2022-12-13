59fc04c6f002e834f7014ace9c5963001b3062a9
Shigal <shiglaws@gmail.com>
2022-12-13 11:25:45 +0530

    found problem

--- src/main/java/fibonacci/Fibonacci.java
+++ src/main/java/fibonacci/Fibonacci.java
@@ -5,6 +5,7 @@ package fibonacci;/*
  */
 
 public class Fibonacci {
+    // problem with this algorithm is we keep recalculating the fibonacci number
     public static void main(String[] args) {
         int n = 50; // took so long
         System.out.println(fibonacci(n));
