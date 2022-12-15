ebb24a498e4c2703fe6a6252beb7c7b4ede7608e
Shigal <shiglaws@gmail.com>
2022-12-15 08:28:58 +0530

    negative number for larger integer

--- src/main/java/fibonacci/Fibonacci.java
+++ src/main/java/fibonacci/Fibonacci.java
@@ -7,6 +7,7 @@ package fibonacci;/*
 public class Fibonacci {
     // problem with this algorithm is we keep recalculating the fibonacci number
     // number of calculations it needs to do increases exponentially as the number becomes larger
+    // even if we redo the calcuations we aren't saving it somewhere so that we can access it later
     public static void main(String[] args) {
         int n = 50; // took so long
         System.out.println(fibonacci(n));
